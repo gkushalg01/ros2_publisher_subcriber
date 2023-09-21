772738d59611071429e443468373f5fc1c5525ee
gkushalg01 <gkushalg01@gmail.com>
2023-09-21 16:56:20 +0530

    publisher and subcriber rolled into one

--- pubsub/src/pubsub.cpp
+++ pubsub/src/pubsub.cpp
@@ -2,9 +2,10 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cstdlib>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -13,32 +14,37 @@ class cPubSub : public rclcpp::Node
 {
   public:
     cPubSub()
-    : Node("publisher_subscriber"), count_(0)
+    : Node("observer"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&cPubSub::timer_callback, this));
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&cPubSub::topic_callback, this, _1));
+      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+      timer_ = this->create_wall_timer(500ms, std::bind(&cPubSub::publisher_callback, this));
+      subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&cPubSub::subcriber_callback, this, _1));
     }
 
   private:
-    void timer_callback()
+    void publisher_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      auto message = geometry_msgs::msg::Twist();
+      message.linear.x = rand()%100;
+      message.linear.y = rand()%100;
+      message.linear.z = rand()%100;
+      message.angular.x = rand()%100;
+      message.angular.y = rand()%100;
+      message.angular.z = rand()%100;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%d'\n linear {x: %f, y: %f, z: %f}; \n angular {x: %f, y: %f, z: %f};", 
+      ++count_, message.linear.x, message.linear.y, message.linear.z, message.angular.x, message.angular.y, message.angular.z);
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     size_t count_;
 
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    void subcriber_callback(const geometry_msgs::msg::Twist::SharedPtr message) const
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      RCLCPP_INFO(this->get_logger(), "I heard: '%d' \n linear {x: %f, y: %f, z: %f}; \n angular {x: %f, y: %f, z: %f};", 
+      count_, message->linear.x, message->linear.y, message->linear.z, message->angular.x, message->angular.y, message->angular.z);
     }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
 
 };
 
